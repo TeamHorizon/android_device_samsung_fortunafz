17963d96dc848dc3d3632d5670fa722a65042483
HassanSardar <hassansardar08@live.com>
2017-05-16 16:39:18 +0000

    Xenonify

--- lineage.mk
+++
@@ -1,9 +0,0 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/fortunafz/full_fortunafz.mk)
-
-# Setup device configuration
-PRODUCT_DEVICE := fortunafz
-PRODUCT_NAME := lineage_fortunafz
